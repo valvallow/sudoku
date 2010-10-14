5faa0922d7cbd20349171e55cc3b6388b7de2568
valvallow <swagger.along.val.valow@gmail.com>
2010-10-14 21:22:11 +0900

    naked single solve

--- sudoku.scm
+++ sudoku.scm
@@ -1,31 +1,10 @@
 ;; sudoku - Wikipedia - http://ja.wikipedia.org/wiki/%E6%95%B0%E7%8B%AC
 
-(define test-data
-  '((7 8 0 5 2 0 0 0 6)
-    (9 0 0 0 0 3 0 7 5)
-    (0 0 5 0 0 6 0 4 0)
-    (0 0 4 0 6 1 3 0 2)
-    (1 0 0 9 0 0 0 0 7)
-    (8 0 2 0 7 0 6 0 0)
-    (0 5 0 2 0 0 8 0 0)
-    (6 3 0 4 0 0 0 0 1)
-    (2 0 0 0 3 7 0 5 9)))
-
-(define test-data-ans
-  '((7 8 1 5 2 4 9 3 6)
-    (9 4 6 1 8 3 2 7 5)
-    (3 2 5 7 9 6 1 4 8)
-    (5 7 4 8 6 1 3 9 2)
-    (1 6 3 9 4 2 5 8 7)
-    (8 9 2 3 7 5 6 1 4)
-    (4 5 7 2 1 9 8 6 3)
-    (6 3 9 4 5 8 7 2 1)
-    (2 1 8 6 3 7 4 5 9)))
-
 (use srfi-1) ; drop, split-at
 (use util.list) ; slices
 (use liv.matrix) ; matrix-*, *-matrix
-(use liv.list) ; 
+(use liv.onlisp.list) ; single?
+
 
 (define-constant region-size 3)
 (define-constant fullset-numbers (iota (* region-size region-size) 1))
@@ -55,16 +34,49 @@
           (receive (took dropped)(split-at m size)
             (rec dropped (cons (apply map append took) acc)))))))
 
-(define (
+(define (candidates matrix x y . keywords)
+  (let-keywords* keywords ((eq? =)
+                           (empty? zero?))
+    (let1 f (pa$ filter (complement empty?))
+      (let* ((row-candidates (f (matrix-row-ref matrix y)))
+             (col-candidates (f (matrix-col-ref matrix x)))
+             (region-candidates (f (region-ref (matrix->regions matrix region-size)
+                                               (region-index x y)))))
+        (lset-difference eq? fullset-numbers row-candidates
+                         col-candidates region-candidates)))))
 
-(define (candidates matrix x y)
-  (let1 f (pa$ filter (complement zero?))
-    (let* ((row-candidates (f (matrix-row-ref matrix y)))
-           (col-candidates (f (matrix-col-ref matrix x)))
-           (region-candidates (f (region-ref (matrix->regions matrix region-size)
-                                             (region-index x y)))))
-      (lset-difference = fullset-numbers row-candidates
-                       col-candidates region-candidates))))
+(define (fix-naked-singles matrix . keywords)
+  (let-keywords* keywords ((empty? zero?)
+                           (car car))
+    (let1 exist? #f
+        (values (map-matrix-with-index
+                 (lambda (e x y)
+                   (if (empty? e)
+                       (let1 can (candidates matrix x y)
+                         (if (single? can)
+                             (begin
+                               (unless exist?
+                                 (set! exist? (not exist?)))
+                               (car can))
+                             e))
+                       e)) matrix)
+                exist?))))
+
+(define (has-empty? matrix :optional (empty? zero?))
+  (let/cc hop
+    (fold (lambda (row acc)
+            (if-let1 it (any empty? row)
+                     (hop it)
+                     acc)) #f matrix)))
+
+(define (fix-naked-singles-solver matrix
+                                  :optional (more (lambda _ 'dead)))
+  (let rec ((m matrix))
+    (receive (cand exist?)(fix-naked-singles m)
+      (cond (exist? (rec cand))
+            ((has-empty? cand)(more cand))
+            (else cand)))))
+
 
 
 
